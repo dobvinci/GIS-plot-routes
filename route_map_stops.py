df6409b910ee102b471aae6a22c191dbfd1d0c1e
victor <vic@test.com>
2019-03-30 22:33:46 +0300

    added 4 files

---
+++ route_map_stops.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 21:33:52 2019
+
+@author: dobvinci
+"""
+
+
+import gpxpy
+import matplotlib.pyplot as plt
+import datetime
+from geopy import distance
+from math import sqrt, floor
+import numpy as np
+import pandas as pd
+import plotly.plotly as py
+import plotly.graph_objs as go
+import haversine
+
+gpx_file = open('route1.gpx', 'r')
+gpx = gpxpy.parse(gpx_file)
+len(gpx.tracks)
+len(gpx.tracks[0].segments)
+len(gpx.tracks[0].segments[0].points)
+data = gpx.tracks[0].segments[0].points
+## Start Position
+start = data[0]
+## End Position
+finish = data[-1]
+df = pd.DataFrame(columns=['lon', 'lat'])
+
+for point in data:
+    df = df.append({'lon': point.longitude, 'lat' : point.latitude}, ignore_index=True)
+plt.plot(df['lat'],df['lon'])
+plt.ylabel("longitude")
+plt.xlabel("Latitude")
+plt.title("Route from Safaricom HQ to furthest BTS  (With stops)")
